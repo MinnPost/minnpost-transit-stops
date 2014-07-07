8f79abb4d0ca1e1dc0385c380d1e9ae6da073890
Alan Palazzolo <apalazzolo@minnpost.com>
2014-07-07 12:05:38 -0500

    Processing stop data by neighborhood.

--- Makefile
+++ Makefile
@@ -12,11 +12,13 @@ build := $(data)/build
 processing := $(data)/processing
 
 # Scripts
-script_tracts := $(processing)/some-data-processing-script.js
+script_tracts := $(processing)/stop-processing.js
 
 # Sources
 source_stops := ftp://gisftp.metc.state.mn.us/TransitStops.zip
 source_boardings := ftp://gisftp.metc.state.mn.us/TransitStopsBoardingsAndAlightings.zip
+source_mpls_n := https://communities.socrata.com/api/geospatial/7sug-nju9?method=export&format=Original
+source_demo := http://www.mncompass.org/_data/neighborhood-profiles/mnc-2011-neighborhood-profiles-alldata-no-suppression-r2.zip
 
 # Local sources
 local_stops_zip := $(original)/transit-stops.zip
@@ -26,14 +28,23 @@ local_boardings_zip := $(original)/transit-boardings.zip
 local_boardings_dir := $(original)/transit-boardings/
 local_boardings := $(original)/transit-boardings/TransitStopsBoardingsAndAlightings.xlsx
 local_shelters := $(original)/Shelter_database.xlsx
+local_mpls_n_zip := $(original)/minneapolis-neighborhoods-2013.zip
+local_mpls_n_dir := $(original)/minneapolis-neighborhoods-2013/
+local_mpls_n := $(original)/minneapolis-neighborhoods-2013/minneapolis-neighborhoods-2013.shp
+local_demo_zip := $(original)/neighborhood_demographics.zip
+local_demo_dir := $(original)/neighborhood_demographics/
+local_demo := $(original)/neighborhood_demographics/MNC_2011_NeighborhoodProfiles_AllData_NoSuppressionR2.xlsx
 
 # Converted
 build_stops := $(build)/stops.geo.json
-build_boardings := $(build)/boardings.csv
+build_boardings := $(build)/boardings.json
 build_shelters := $(build)/shelters.csv
+build_mlps_n := $(build)/minneapolis-neighborhoods.geo.json
+build_demo := $(build)/neighborhood-demographics.json
+build_stops_n := $(build)/neighborhood-stop-data.geo.json
 
 # Final
-stops := $(data)/stops.geo.json
+stops := $(data)/neighborhood-stop-data.topo.json
 
 
 
@@ -51,40 +62,60 @@ $(local_boardings):
 	unzip $(local_boardings_zip) -d $(local_boardings_dir)
 	touch $(local_boardings)
 
-download: $(local_stops) $(local_boardings)
+$(local_mpls_n):
+	mkdir -p $(original)
+	curl -o $(local_mpls_n_zip) "$(source_mpls_n)"
+	unzip $(local_mpls_n_zip) -d $(local_mpls_n_dir)
+	touch $(local_mpls_n)
+
+$(local_demo):
+	mkdir -p $(original)
+	curl -o $(local_demo_zip) "$(source_demo)"
+	unzip $(local_demo_zip) -d $(local_demo_dir)
+	touch $(local_demo)
+
+download: $(local_stops) $(local_boardings) $(local_mpls_n) $(local_demo)
 clean_download:
 	rm -rv $(local_stops_dir) $(local_stops_zip)
 	rm -rv $(local_boardings_dir) $(local_boardings_zip)
+	rm -rv $(local_mpls_n_dir) $(local_mpls_n_zip)
 
 
 # Convert and filter data files
 $(build_boardings): $(local_boardings)
 	mkdir -p $(build)
-	in2csv $(local_boardings) --format=xlsx > $(build_boardings)
+	in2csv $(local_boardings) --format=xlsx | csvjson > $(build_boardings)
 
 $(build_shelters): $(local_shelters)
 	mkdir -p $(build)
 	in2csv $(local_shelters) --format=xlsx > $(build_shelters)
 
-$(build_stops): $(local_stops) $(build_boardings) $(build_shelters)
+$(build_demo): $(local_demo)
+	mkdir -p $(build)
+	in2csv $(local_demo) --format=xlsx --no-header-row | csvcut --no-header-row -c 1,2,3,4,5,6,16,46,56,58,132,238,434 | csvjson > $(build_demo)
+
+$(build_mlps_n): $(local_mpls_n)
+	mkdir -p $(build)
+	ogr2ogr -f "GeoJSON" $(build_mlps_n) $(local_mpls_n) -overwrite -t_srs "EPSG:4326"
+
+$(build_stops): $(local_stops) $(build_shelters)
 	mkdir -p $(build)
 	ogr2ogr -f "GeoJSON" $(build_stops) $(local_stops) -overwrite -t_srs "EPSG:4326" \
 		-sql "SELECT TransitStops.site_id AS id, TransitStops.site_on AS street, TransitStops.site_at AS at, TransitStops.publiccomm AS comments, TransitStops.dt_zone AS dt, TransitStops.bench AS bench, TransitStops.parkride AS parkride, TransitStops.sidewalk AS sidewalk, TransitStops.adaaccess AS ada_access, TransitStops.ada_comm AS ada_comments, TransitStops.sign AS sign, TransitStops.shelter AS shelter, TransitStops.hi_freq AS freq, shelters.PROP_DESC AS shelter_desc, shelters.OWNER AS owner, shelters.shel_type AS shelter_type, shelters.Light AS light, shelters.heat AS heat FROM TransitStops LEFT JOIN '$(build_shelters)'.shelters ON TransitStops.site_id = shelters.site_id WHERE TransitStops.busstop_yn = 'Y'"
 
-convert: $(build_boardings) $(build_shelters) $(build_stops)
+$(build_stops_n): $(build_boardings) $(build_shelters) $(build_demo) $(build_mlps_n) $(build_stops)
+	node $(script_tracts)
+
+$(stops): $(build_stops_n)
+	topojson $(build_stops_n) --id-property="id" -p -o $(stops)
+
+convert: $(stops)
 clean_convert:
 	rm -rv $(build)/*
+	rm $(stops)
 
-
-# Final
-$(stops): $(build_stops)
-	cp $(build_stops) $(stops)
 
 
 # General
 all: convert
 clean: clean_download clean_convert
-
-
-
-ogr2ogr -overwrite -sql "select TransitStops.*, stops.*, stops.Light AS light from TransitStops left join 'stops.csv'.stops on TransitStops.site_id = stops.site_id" T2/combine_stops.shp TransitStops/TransitStops.shp
---
+++ data/processing/stop-processing.js
@@ -0,0 +1,187 @@
+/**
+ * Processes stops into shapes and does some analysis.
+ */
+
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+var gu = require('geojson-utils');
+var ga = require('geojson-area');
+
+var stops = require('../build/stops.geo.json')
+var boardings = require('../build/boardings.json')
+var mplsN = require('../build/minneapolis-neighborhoods.geo.json')
+var demographics = require('../build/neighborhood-demographics.json');
+
+var output = path.resolve(__dirname, '../build/neighborhood-stop-data.geo.json');
+
+// Translation of demographic neighborhood name to our neighborhood id
+function translateNeighborhood(name) {
+  var id = name;
+  var translations = {
+    'cedar_riverside_west_bank': 'cedar_riverside',
+    'prospect_park_east_river_road': 'prospect_park_east_river',
+    'u_of_mn': 'university_of_minnesota',
+    'jordon': 'jordan',
+    'stevens_square_loring_hgts_': 'stevens_square_loring_heights',
+    'northrup': 'northrop',
+    'mid_city_ind_area': 'mid_city_industrial',
+    'nicollet_island': 'nicollet_island_east_bank',
+    'humboldt_industrial': 'humboldt_industrial_area'
+  };
+
+  id = id.toLowerCase().trim().replace(/[^\w ]+/g,'').replace(/ +/g,'_').replace(/[^\w-]+/g,'');
+  // Manual translation
+  if (translations[id]) {
+    id = translations[id];
+  }
+
+  return id;
+}
+
+// Ouput message
+console.log('Combining stop data into neighborhoods...');
+
+// Group boardings for easier lookup
+boardings = _.groupBy(boardings, 'Site_id');
+
+// Combine boarding data to stop data
+stops.features = _.map(stops.features, function(f, fi) {
+  var id = f.properties.id;
+
+  if (boardings[id]) {
+    // Scheduled trips
+    f.properties.trips = _.reduce(boardings[id], function(memo, b, bi) {
+      return (memo + parseInt(b.Trips, 10));
+    }, 0);
+    // Observed trips for data
+    f.properties.tripsO = _.reduce(boardings[id], function(memo, b, bi) {
+      return (memo + parseInt(b.Trips_Obs, 10));
+    }, 0);
+    // Ons and offs
+    f.properties.ons = _.reduce(boardings[id], function(memo, b, bi) {
+      return (memo + parseFloat(b.Ons));
+    }, 0);
+    f.properties.offs = _.reduce(boardings[id], function(memo, b, bi) {
+      return (memo + parseFloat(b.Offs));
+    }, 0);
+  }
+
+  return f;
+});
+
+
+// Aggregate into polygons
+mplsN.features = _.map(mplsN.features, function(f, fi) {
+  var p = f.properties
+
+  // Get area
+  p.area_m = ga.geometry(f.geometry);
+  p.area_km = p.area_m / 1000 / 1000;
+
+  // Look at each stop and determine if in polygon
+  _.each(stops.features, function(s, si) {
+    if (gu.pointInPolygon(s.geometry, f.geometry)) {
+      p.stops = p.stops || [];
+      p.stops.push(s);
+    }
+  });
+
+  // Reduce data into polygons
+  p.s_count = p.stops.length;
+  p.heat = _.reduce(p.stops, function(memo, s, si) {
+    return (s.properties.heat == 'Yes') ? memo + 1 : memo;
+  }, 0);
+  p.shelter = _.reduce(p.stops, function(memo, s, si) {
+    return (s.properties.shelter == 'Y') ? memo + 1 : memo;
+  }, 0);
+  p.light = _.reduce(p.stops, function(memo, s, si) {
+    return (s.properties.light == 'Yes') ? memo + 1 : memo;
+  }, 0);
+  p.bench = _.reduce(p.stops, function(memo, s, si) {
+    return (s.properties.bench == 'Y') ? memo + 1 : memo;
+  }, 0);
+  p.sign = _.reduce(p.stops, function(memo, s, si) {
+    return (s.properties.sign == 'Y') ? memo + 1 : memo;
+  }, 0);
+  p.trips = _.reduce(p.stops, function(memo, s, si) {
+    return (s.properties.trips) ? memo + s.properties.trips : memo;
+  }, 0);
+  p.tripsO = _.reduce(p.stops, function(memo, s, si) {
+    return (s.properties.tripsO) ? memo + s.properties.tripsO : memo;
+  }, 0);
+  p.ons = _.reduce(p.stops, function(memo, s, si) {
+    return (s.properties.ons) ? memo + s.properties.ons : memo;
+  }, 0);
+  p.offs = _.reduce(p.stops, function(memo, s, si) {
+    return (s.properties.offs) ? memo + s.properties.offs : memo;
+  }, 0);
+
+  // Make overall stop score
+  p.score = (
+    (p.s_count * 0.25) +
+    (p.sign * 1) +
+    (p.bench * 1) +
+    (p.shelter * 2) +
+    (p.light * 3) +
+    (p.heat * 4)
+  );
+
+  // Remove stops from data
+  delete p.stops
+
+  // Add in demographic data
+  var found = false;
+  _.each(demographics, function(d, di) {
+    if (d.column2 && d.column5 === 'Minneapolis' && translateNeighborhood(d.column2) === p.neighbor_1) {
+      found = true;
+      p.population = parseInt(d.column6, 10);
+      p.employed = parseInt(d.column238, 10);
+      p.employed_per = p.employed / p.population;
+      p.jobs = parseInt(d.column434, 10);
+      p.non_white_per = 1 - parseFloat(d.column16);
+    }
+  });
+  if (!found) {
+    console.log('Could not find neighborhood demo match for: ');
+    console.log(p);
+  }
+
+  // Adjust for demographics
+  p.score_by_pop = p.score / p.population;
+  p.score_by_emp = p.score / p.employed;
+  p.score_by_jobs = p.score / p.jobs;
+
+  // Adjust for ridership
+  p.score_by_trips = p.score / p.trips;
+  p.score_by_ons = p.score / p.ons;
+  p.score_by_offs = p.score / p.offs;
+  p.score_by_ons_offs = p.score / (p.offs + p.ons);
+
+  // Adjust for area
+  p.score_by_area = p.score / p.area_km;
+  p.score_by_pop_area = p.score / p.population / p.area_km;
+
+  return f;
+});
+
+// Just some test output
+/*
+var testProp = 'score_by_area';
+mplsN.features = _.sortBy(mplsN.features, function(f, fi) {
+  return f.properties[testProp];
+});
+_.each(mplsN.features, function(f, fi) {
+  console.log(f.properties.Name + ': '  + f.properties[testProp]);
+});
+*/
+
+// Save output
+fs.writeFile(output, JSON.stringify(mplsN), function(err) {
+  if (err) {
+    console.log('Issue writing file: ' + err);
+  }
+  else {
+    console.log('GeoJSON file saved to: ' + output);
+  }
+});
